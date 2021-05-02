a51202ecfe0efe295b1255b827aa599c5d0612a5
Kohei Morita <mrtc0@ssrf.in>
2021-05-02 23:03:50 +0900

    Permissions Policy
    Signed-off-by: Kohei Morita <mrtc0@ssrf.in>

---
+++ web/permissions-policy/index.php
@@ -0,0 +1,11 @@
+<?php
+header("Feature-Policy: fullscreen *");
+?>
+
+<html>
+  <head>
+  </head>
+  <body>
+    <iframe src="http://localhost:3000/frame.html" allow="fullscreen"></iframe>
+  </body>
+</html>
